338e9e594910f708159fe3519cc8b147f2e2709d
matt <53015609+MattBilyeu@users.noreply.github.com>
2023-12-20 10:37:40 -0500

    Client requested updates
    Minor changes for the client

--- src/app/gallery/gallery-item/gallery-item.component.ts
+++ src/app/gallery/gallery-item/gallery-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, HostListener } from '@angular/core';
 import { ActivatedRoute, Router, Params } from '@angular/router';
 import { ContentfulService } from 'src/app/shared/contentful.service';
 
@@ -7,18 +7,22 @@ import { ContentfulService } from 'src/app/shared/contentful.service';
   templateUrl: './gallery-item.component.html',
   styleUrls: ['./gallery-item.component.css']
 })
+
 export class GalleryItemComponent implements OnInit {
   popUpActive: boolean = false;
   popUpIndex: number = 0;
   activatedEntry: any;
   entryID: string = '';
   allImages: string[] = [];
+  isMobile!: boolean;
 
   constructor(private contentfulService: ContentfulService,
               private router: Router,
-              private route: ActivatedRoute) {}
+              private route: ActivatedRoute,
+              private elementRef: ElementRef) {}
 
   ngOnInit() {
+    this.checkIfMobile();
     const params = this.route.params.subscribe(
       (params: Params) => {
         this.entryID = params['id']
@@ -35,6 +39,16 @@ export class GalleryItemComponent implements OnInit {
           }
         });
       })
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkIfMobile();
+  }
+
+  checkIfMobile() {
+    const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;;
+    this.isMobile = width < 768;
   }
 
   disablePopUp() {
@@ -67,7 +81,7 @@ export class GalleryItemComponent implements OnInit {
     this.router.navigate(['']);
   }
 
-  getRelativeUrl(path: string) {
-    return `https://sovereign-development.netlify.app/gallery/#${path}`
+  getRelativeUrl(id: string) {
+    return `https://sovereign-development.netlify.app/gallery/#images/${id}`
   }
 }
